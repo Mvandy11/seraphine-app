ece38f06554f9481d667cf1ac91ed75c65085d72
Mvandy11 <m_vandeventer@yahoo.com>
2026-04-18 03:12:08 +0000

    Fix SeraphinePortrait image path for Netlify production

--- SeraphinePortrait.tsx
+++ SeraphinePortrait.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from "react";
-import seraphineImg from "@/assets/seraphine.jpeg";
 import "./SeraphinePortrait.css";
 
 export default function SeraphinePortrait() {
@@ -25,9 +24,14 @@ export default function SeraphinePortrait() {
   return (
     <div className="seraphine-portrait-wrapper">
       <div ref={containerRef} className="seraphine-portrait-container">
-        <img src={seraphineImg} alt="Seraphine" className="seraphine-portrait" />
+        <img
+          src="/art/seraphine/portrait.png"
+          alt="Seraphine"
+          className="seraphine-portrait"
+        />
         <div className="seraphine-aura"></div>
       </div>
     </div>
   );
 }
+
